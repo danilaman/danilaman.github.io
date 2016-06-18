8e4b964120d2c7a3d61a0ee333c633cbb70fc31c
Danila <manturov2000@yandex.ru>
2016-06-18 10:11:09 +0400

    пвпвп

---
+++ stroyloft/js/platform.js
@@ -0,0 +1,26 @@
+$(document).ready(function()
+{
+	$('.oneService').hover(function(){
+
+
+	var bw = $(this).find(".serviceimg").css("backgroundImage")
+	var cl = $(this).find(".serviceimg").attr("servicePhoto")
+
+	window.bw = bw
+	window.cl = cl
+
+		$(this).find(".serviceimg").css({"background": "url(./images/"+cl+") no-repeat center center", "transition": "0.5s", "-moz-background-size": "cover",
+	"background-size": "cover", "wudth": "200px", "height": "200px"
+		})
+
+		$(this).find(".examplesGreen").slideToggle(300)
+	},
+	function()
+	{
+		$(this).find(".serviceimg").css({"background": bw + "no-repeat center center", "transition": "0.5s", "-moz-background-size": "cover",
+	"background-size": "cover", "wudth": "170px", "height": "170px"
+	})
+		$(this).find(".examplesGreen").slideToggle(300)
+
+})
+})
