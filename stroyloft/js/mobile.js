3f12de44d304e013e4cecbfec4bdc8dbf9c7d113
Danila <manturov2000@yandex.ru>
2016-07-11 13:10:18 +0300

    павправ

--- stroyloft/js/mobile.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function()
-{
-	$("body").css("width", $(window).width())
-	$("#navbar").css("height", $(window).height()*0.14)
-	$("#mainlogo").css({
-		"width": $(window).width()*0.7,
-		"margin": $(window).height()*0.02 +"px "+ $(window).width()*0.01 +"px"
-})
-})
